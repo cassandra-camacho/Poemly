5cea2eb19e7eabc9518a91717fcf6dec8e884602
Cassie C <cassandra22x@gmail.com>
2025-03-20 18:30:28 -0400

    Initial Commit

---
+++ Poemly/PoemlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoemlyApp.swift
+//  Poemly
+//
+//  Created by Cassandra Camacho on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PoemlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PoemlyTests/PoemlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PoemlyTests.swift
+//  PoemlyTests
+//
+//  Created by Cassandra Camacho on 3/20/25.
+//
+
+import Testing
+@testable import Poemly
+
+struct PoemlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
