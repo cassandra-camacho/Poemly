a2cb2a6bc9d27a28bf50634750413a801c80f761
Saverio Negro <saverionegro@gmail.com>
2025-03-21 02:09:30 +0100

    Change Title for first poem.

--- Poemly/ViewModels/PoemManager.swift
+++ Poemly/ViewModels/PoemManager.swift
@@ -10,7 +10,7 @@ import SwiftUI
 class PoemManager {
     
     let poems: [Poem] = [
-        Poem(title: "Day Appreciation", text: "How Beautiful the day is!", prompt: "Write Peom about the day.", date: .now),
+        Poem(title: "Appreciation Day", text: "How Beautiful the day is!", prompt: "Write Peom about the day.", date: .now),
         Poem(title: "Spring Blossoms", text: "Spring blossoms bring you to me", prompt: "Reflect on this first day of spring.", date: Calendar.current.date(byAdding: .day, value: 1, to: .now)!),
         Poem(title: "A Thousands Mornings with You", text: "Your morning begins with your scent", prompt: "Reflect on someone you would like to spend most of your time with.", date: Calendar.current.date(byAdding: .day, value: 2, to: .now)!)
     ]
